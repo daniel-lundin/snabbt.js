e1972c9317521f375e8b15533da51f1293aed701
daniel-lundin <daniel.t.lundin@gmail.com>
2015-01-07 20:24:11 +0100

    Fix jquery bug after umd fixes

--- src/jquery.snabbt.js
+++ src/jquery.snabbt.js
@@ -1,8 +1,7 @@
 if(window.jQuery) {
   (function ( $ ) {
     $.fn.snabbt = function(arg1, arg2) {
-
-      return snabbt(this.get(), arg1, arg2);
+      return snabbtjs.snabbt(this.get(), arg1, arg2);
     };
   }( jQuery ));
 }
