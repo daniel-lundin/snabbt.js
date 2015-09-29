ebc7fd6119146f3f3b3c5d446d68be1870a876bf
daniel-lundin <daniel.t.lundin@gmail.com>
2015-09-29 07:52:03 +0200

    Started browserifying, WIP

---
+++ src/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var createMatrix = require('./matrix.js');
+
+
+module.exports = function() {
+  console.log('this is snabbt');
+  createMatrix();
+}
---
+++ src/matrix.js
@@ -0,0 +1,257 @@
+'use strict';
+
+function assignedMatrixMultiplication(a, b, res) {
+  // Unrolled loop
+  res[0] = a[0] * b[0] + a[1] * b[4] + a[2] * b[8] + a[3] * b[12];
+  res[1] = a[0] * b[1] + a[1] * b[5] + a[2] * b[9] + a[3] * b[13];
+  res[2] = a[0] * b[2] + a[1] * b[6] + a[2] * b[10] + a[3] * b[14];
+  res[3] = a[0] * b[3] + a[1] * b[7] + a[2] * b[11] + a[3] * b[15];
+
+  res[4] = a[4] * b[0] + a[5] * b[4] + a[6] * b[8] + a[7] * b[12];
+  res[5] = a[4] * b[1] + a[5] * b[5] + a[6] * b[9] + a[7] * b[13];
+  res[6] = a[4] * b[2] + a[5] * b[6] + a[6] * b[10] + a[7] * b[14];
+  res[7] = a[4] * b[3] + a[5] * b[7] + a[6] * b[11] + a[7] * b[15];
+
+  res[8] = a[8] * b[0] + a[9] * b[4] + a[10] * b[8] + a[11] * b[12];
+  res[9] = a[8] * b[1] + a[9] * b[5] + a[10] * b[9] + a[11] * b[13];
+  res[10] = a[8] * b[2] + a[9] * b[6] + a[10] * b[10] + a[11] * b[14];
+  res[11] = a[8] * b[3] + a[9] * b[7] + a[10] * b[11] + a[11] * b[15];
+
+  res[12] = a[12] * b[0] + a[13] * b[4] + a[14] * b[8] + a[15] * b[12];
+  res[13] = a[12] * b[1] + a[13] * b[5] + a[14] * b[9] + a[15] * b[13];
+  res[14] = a[12] * b[2] + a[13] * b[6] + a[14] * b[10] + a[15] * b[14];
+  res[15] = a[12] * b[3] + a[13] * b[7] + a[14] * b[11] + a[15] * b[15];
+
+  return res;
+}
+
+function assignTranslate(matrix, x, y, z) {
+  matrix[0] = 1;
+  matrix[1] = 0;
+  matrix[2] = 0;
+  matrix[3] = 0;
+  matrix[4] = 0;
+  matrix[5] = 1;
+  matrix[6] = 0;
+  matrix[7] = 0;
+  matrix[8] = 0;
+  matrix[9] = 0;
+  matrix[10] = 1;
+  matrix[11] = 0;
+  matrix[12] = x;
+  matrix[13] = y;
+  matrix[14] = z;
+  matrix[15] = 1;
+}
+
+function assignRotateX(matrix, rad) {
+  matrix[0] = 1;
+  matrix[1] = 0;
+  matrix[2] = 0;
+  matrix[3] = 0;
+  matrix[4] = 0;
+  matrix[5] = Math.cos(rad);
+  matrix[6] = -Math.sin(rad);
+  matrix[7] = 0;
+  matrix[8] = 0;
+  matrix[9] = Math.sin(rad);
+  matrix[10] = Math.cos(rad);
+  matrix[11] = 0;
+  matrix[12] = 0;
+  matrix[13] = 0;
+  matrix[14] = 0;
+  matrix[15] = 1;
+}
+
+
+var assignRotateY = function(matrix, rad) {
+  matrix[0] = Math.cos(rad);
+  matrix[1] = 0;
+  matrix[2] = Math.sin(rad);
+  matrix[3] = 0;
+  matrix[4] = 0;
+  matrix[5] = 1;
+  matrix[6] = 0;
+  matrix[7] = 0;
+  matrix[8] = -Math.sin(rad);
+  matrix[9] = 0;
+  matrix[10] = Math.cos(rad);
+  matrix[11] = 0;
+  matrix[12] = 0;
+  matrix[13] = 0;
+  matrix[14] = 0;
+  matrix[15] = 1;
+};
+
+function assignRotateZ(matrix, rad) {
+  matrix[0] = Math.cos(rad);
+  matrix[1] = -Math.sin(rad);
+  matrix[2] = 0;
+  matrix[3] = 0;
+  matrix[4] = Math.sin(rad);
+  matrix[5] = Math.cos(rad);
+  matrix[6] = 0;
+  matrix[7] = 0;
+  matrix[8] = 0;
+  matrix[9] = 0;
+  matrix[10] = 1;
+  matrix[11] = 0;
+  matrix[12] = 0;
+  matrix[13] = 0;
+  matrix[14] = 0;
+  matrix[15] = 1;
+}
+
+function assignSkew(matrix, ax, ay) {
+  matrix[0] = 1;
+  matrix[1] = Math.tan(ax);
+  matrix[2] = 0;
+  matrix[3] = 0;
+  matrix[4] = Math.tan(ay);
+  matrix[5] = 1;
+  matrix[6] = 0;
+  matrix[7] = 0;
+  matrix[8] = 0;
+  matrix[9] = 0;
+  matrix[10] = 1;
+  matrix[11] = 0;
+  matrix[12] = 0;
+  matrix[13] = 0;
+  matrix[14] = 0;
+  matrix[15] = 1;
+}
+
+
+function assignScale(matrix, x, y) {
+  matrix[0] = x;
+  matrix[1] = 0;
+  matrix[2] = 0;
+  matrix[3] = 0;
+  matrix[4] = 0;
+  matrix[5] = y;
+  matrix[6] = 0;
+  matrix[7] = 0;
+  matrix[8] = 0;
+  matrix[9] = 0;
+  matrix[10] = 1;
+  matrix[11] = 0;
+  matrix[12] = 0;
+  matrix[13] = 0;
+  matrix[14] = 0;
+  matrix[15] = 1;
+}
+
+function assignIdentity(matrix) {
+  matrix[0] = 1;
+  matrix[1] = 0;
+  matrix[2] = 0;
+  matrix[3] = 0;
+  matrix[4] = 0;
+  matrix[5] = 1;
+  matrix[6] = 0;
+  matrix[7] = 0;
+  matrix[8] = 0;
+  matrix[9] = 0;
+  matrix[10] = 1;
+  matrix[11] = 0;
+  matrix[12] = 0;
+  matrix[13] = 0;
+  matrix[14] = 0;
+  matrix[15] = 1;
+}
+
+function copyArray(a, b) {
+  b[0] = a[0];
+  b[1] = a[1];
+  b[2] = a[2];
+  b[3] = a[3];
+  b[4] = a[4];
+  b[5] = a[5];
+  b[6] = a[6];
+  b[7] = a[7];
+  b[8] = a[8];
+  b[9] = a[9];
+  b[10] = a[10];
+  b[11] = a[11];
+  b[12] = a[12];
+  b[13] = a[13];
+  b[14] = a[14];
+  b[15] = a[15];
+}
+
+var createMatrix = function() {
+    var data = new Float32Array(16);
+    var a = new Float32Array(16);
+    var b = new Float32Array(16);
+    assignIdentity(data);
+
+    return {
+      data: data,
+
+      asCSS: function() {
+        var css = 'matrix3d(';
+        for (var i = 0; i < 15; ++i) {
+          if (Math.abs(data[i]) < 0.0001) {
+            css += '0,';
+           } else {
+             css += data[i].toFixed(10) + ',';
+           }
+        }
+        if (Math.abs(data[15]) < 0.0001) {
+          css += '0)';
+        } else {
+          css += data[15].toFixed(10) + ')';
+        }
+        return css;
+      },
+
+      clear: function() {
+        assignIdentity(data);
+      },
+
+      translate: function(x, y, z) {
+        copyArray(data, a);
+        assignTranslate(b, x, y, z);
+        assignedMatrixMultiplication(a, b, data);
+        return this;
+      },
+
+      rotateX: function(radians) {
+        copyArray(data, a);
+        assignRotateX(b, radians);
+        assignedMatrixMultiplication(a, b, data);
+        return this;
+      },
+
+      rotateY: function(radians) {
+        copyArray(data, a);
+        assignRotateY(b, radians);
+        assignedMatrixMultiplication(a, b, data);
+        return this;
+      },
+
+      rotateZ: function(radians) {
+        copyArray(data, a);
+        assignRotateZ(b, radians);
+        assignedMatrixMultiplication(a, b, data);
+        return this;
+      },
+
+      scale: function(x, y) {
+        copyArray(data, a);
+        assignScale(b, x, y);
+        assignedMatrixMultiplication(a, b, data);
+        return this;
+      },
+
+    skew: function(ax, ay) {
+      copyArray(data, a);
+      assignSkew(b, ax, ay);
+      assignedMatrixMultiplication(a, b, data);
+      return this;
+    }
+  };
+};
+
+
+module.exports = createMatrix;
