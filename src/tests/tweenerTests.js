facd6cff2ff64efdc7f65d0ef945d21dd8c89762
daniel-lundin <daniel.t.lundin@gmail.com>
2015-10-02 19:59:06 +0200

    Added basic tweenerTests

---
+++ src/tests/tweenerTests.js
@@ -0,0 +1,55 @@
+'use strict';
+
+var expect = require('chai').expect;
+var tweeners = require('../tweeners.js');
+
+function createState(props, value) {
+  var state = {};
+  props.forEach((prop) => {
+    state[prop] = value;
+  });
+  return state;
+}
+
+describe('tweeners', () => {
+  describe('stateTweener', () => {
+    it('should tween 3d properties that are present', () => {
+      var props = ['position', 'rotation'];
+      var startState = createState(props, [1, 1, 1]);
+      var endState = createState(props, [2, 2, 2]);
+      var result = createState(props, [0, 0, 0]);
+
+      var tweener = tweeners.createStateTweener(startState, endState, result);
+      tweener.tween(0.5);
+
+      expect(result.position).to.eql([1.5, 1.5, 1.5]);
+      expect(result.rotation).to.eql([1.5, 1.5, 1.5]);
+    });
+
+    it('should tween 2d properties that are present', () => {
+      var props = ['skew', 'scale'];
+      var startState = createState(props, [1, 1]);
+      var endState = createState(props,  [2, 2]);
+      var result = createState(props, [0, 0]);
+
+      var tweener = tweeners.createStateTweener(startState, endState, result);
+      tweener.tween(0.5);
+
+      expect(result.skew).to.eql([1.5, 1.5]);
+      expect(result.scale).to.eql([1.5, 1.5]);
+    });
+
+    it('should tween scalar properties that are present', () => {
+      var props = ['opacity', 'width'];
+      var startState = createState(props, 1);
+      var endState = createState(props,  2);
+      var result = createState(props, 0);
+
+      var tweener = tweeners.createStateTweener(startState, endState, result);
+      tweener.tween(0.5);
+
+      expect(result.opacity).to.eql(1.5);
+      expect(result.width).to.eql(1.5);
+    });
+  });
+});
--- src/tweeners.js
+++ src/tweeners.js
@@ -1,93 +1,57 @@
 'use strict';
 
 var matrix = require('./matrix.js');
+var props = require('./properties').tweenableProperties;
+var types = require('./properties').types;
 
 function createStateTweener(startState, endState, resultState) {
   var start = startState;
   var end = endState;
   var result = resultState;
 
-  var tweenPosition = end.position !== undefined;
-  var tweenRotation = end.rotation !== undefined;
-  var tweenRotationPost = end.rotationPost !== undefined;
-  var tweenScale = end.scale !== undefined;
-  var tweenScalePost = end.scalePost !== undefined;
-  var tweenSkew = end.skew !== undefined;
-  var tweenWidth = end.width !== undefined;
-  var tweenHeight = end.height !== undefined;
-  var tweenOpacity = end.opacity !== undefined;
+  var tweenProps = [];
+  Object.keys(props).forEach((prop) => {
+    if (endState[prop] !== undefined) {
+      tweenProps.push(prop);
+    }
+  });
 
+  function tween3D(from, to, res, prop, tweenValue) {
+      var dX = to[prop][0] - from[prop][0];
+      var dY = to[prop][1] - from[prop][1];
+      var dZ = to[prop][2] - from[prop][2];
+      res[prop][0] = from[prop][0] + tweenValue * dX;
+      res[prop][1] = from[prop][1] + tweenValue * dY;
+      res[prop][2] = from[prop][2] + tweenValue * dZ;
+  }
+
+  function tween2D(from, to, res, prop, tweenValue) {
+      var dX = to[prop][0] - from[prop][0];
+      var dY = to[prop][1] - from[prop][1];
+      res[prop][0] = from[prop][0] + tweenValue * dX;
+      res[prop][1] = from[prop][1] + tweenValue * dY;
+  }
+
+  function tweenScalar(from, to, res, prop, tweenValue) {
+      var dX = to[prop] - from[prop];
+      res[prop] = from[prop] + tweenValue * dX;
+  }
   // Public API
   return {
 
     tween(tweenValue) {
 
-      if (tweenPosition) {
-        var dX = end.position[0] - start.position[0];
-        var dY = end.position[1] - start.position[1];
-        var dZ = end.position[2] - start.position[2];
-        result.position[0] = start.position[0] + tweenValue * dX;
-        result.position[1] = start.position[1] + tweenValue * dY;
-        result.position[2] = start.position[2] + tweenValue * dZ;
-      }
+      tweenProps.forEach((prop) => {
+        const type = props[prop][0];
+        if (type === types.ARRAY_3) {
+          tween3D(start, end, result, prop, tweenValue);
+        } else if (type === types.ARRAY_2) {
+          tween2D(start, end, result, prop, tweenValue);
+        } else {
+          tweenScalar(start, end, result, prop, tweenValue);
+        }
 
-      if (tweenRotation) {
-        var dAX = end.rotation[0] - start.rotation[0];
-        var dAY = end.rotation[1] - start.rotation[1];
-        var dAZ = end.rotation[2] - start.rotation[2];
-        result.rotation[0] = start.rotation[0] + tweenValue * dAX;
-        result.rotation[1] = start.rotation[1] + tweenValue * dAY;
-        result.rotation[2] = start.rotation[2] + tweenValue * dAZ;
-      }
-
-      if (tweenRotationPost) {
-        var dBX = end.rotationPost[0] - start.rotationPost[0];
-        var dBY = end.rotationPost[1] - start.rotationPost[1];
-        var dBZ = end.rotationPost[2] - start.rotationPost[2];
-        result.rotationPost[0] = start.rotationPost[0] + tweenValue * dBX;
-        result.rotationPost[1] = start.rotationPost[1] + tweenValue * dBY;
-        result.rotationPost[2] = start.rotationPost[2] + tweenValue * dBZ;
-      }
-
-      if (tweenSkew) {
-        var dSkewX = end.skew[0] - start.skew[0];
-        var dSkewY = end.skew[1] - start.skew[1];
-
-        result.skew[0] = start.skew[0] + tweenValue * dSkewX;
-        result.skew[1] = start.skew[1] + tweenValue * dSkewY;
-      }
-
-      if (tweenScale) {
-        var dSX = end.scale[0] - start.scale[0];
-        var dSY = end.scale[1] - start.scale[1];
-
-        result.scale[0] = start.scale[0] + tweenValue * dSX;
-        result.scale[1] = start.scale[1] + tweenValue * dSY;
-      }
-
-      if (tweenScalePost) {
-        var dSXPost = end.scalePost[0] - start.scalePost[0];
-        var dSYPost = end.scalePost[1] - start.scalePost[1];
-
-        result.scalePost[0] = start.scalePost[0] + tweenValue * dSXPost;
-        result.scalePost[1] = start.scalePost[1] + tweenValue * dSYPost;
-      }
-
-
-      if (tweenWidth) {
-        var dWidth = end.width - start.width;
-        result.width = start.width + tweenValue * dWidth;
-      }
-
-      if (tweenHeight) {
-        var dHeight = end.height - start.height;
-        result.height = start.height + tweenValue * dHeight;
-      }
-
-      if (tweenOpacity) {
-        var dOpacity = end.opacity - start.opacity;
-        result.opacity = start.opacity + tweenValue * dOpacity;
-      }
+      });
     },
 
     asMatrix() {
@@ -96,6 +60,10 @@ function createStateTweener(startState, endState, resultState) {
 
     getProperties() {
       return result.getProperties();
+    },
+
+    result() {
+      return result;
     },
 
     setReverse() {
